d42fa63a8a5474c9ae0ca03efe568ab1876e2252
arafatyamin <mdh631987@gmail.com>
2022-08-23 05:12:32 +0600

    common function added

--- js/app.js
+++ js/app.js
@@ -1,28 +1,24 @@
 const cartArray = [];
 
 function display(cartProduct) {
-    // console.log(cartProduct);
     const nameList = document.getElementById('name-list');
     nameList.innerHTML = '';
     for (let i = 0; i < cartProduct.length; i++) {
-        // console.log(i)
-        // console.log(cartProduct[i])
         const li = document.createElement('li');
-        li.innerText = cartProduct[i]
-        nameList.appendChild(li);
+            li.innerText = cartProduct[i]
+            nameList.appendChild(li);
+        
     }
 }
 
 function addToCard(element) {
-    // console.log(element.parentNode);
-    // console.log(element.parentNode.children[0].innerText);
 
     const productName = element.parentNode.children[0].innerText;
-    // console.log(productName);
 
     cartArray.push(productName)
-    // console.log(cartArray.length)
     display(cartArray)
+    
+
 }
 
 // budget calculation
@@ -32,10 +28,8 @@ document.getElementById('calculate-price').addEventListener('click', function ()
     const nameListElement = document.getElementById('name-list');
     const nameLengthString = nameListElement.children.length;
     const nameLength = parseInt(nameLengthString);
-    // console.log(nameLength)
 
     const totalPlayerPrice = perPlayerPrice * nameLength;
-    console.log(totalPlayerPrice)
 
     const playerExpense = document.getElementById('player-expenses');
     playerExpense.innerText = totalPlayerPrice;
@@ -52,7 +46,4 @@ document.getElementById('total-calculate-btn').addEventListener('click', functio
 
     const totalElement = document.getElementById('total-cost');
     totalElement.innerText = totalCost;
-
-    console.log(playerTotalCost, managerCost, coachCost, totalCost);
-
 })
